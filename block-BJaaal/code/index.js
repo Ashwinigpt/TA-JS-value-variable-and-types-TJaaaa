c2ee7fe4e8476f2e6286fdc3f102ce95781d5511
Ashwini Gupta <ashwinigpt1996@gmail.com>
2022-06-22 22:48:18 +0530

    block-BJaaal

--- block-BJaaal/code/index.js
+++ block-BJaaal/code/index.js
@@ -32,7 +32,7 @@ const taxPercent = 10;
   `
   - Above message should be in two lines
 */
-alert(`The total amount is ${amount} \n the tax amount is ${amount} * ${taxPercent} / 100`) ;
+alert(`The total amount is ${amount} \n the tax amount is ${(amount * taxPercent) / 100}`) ;
 
 
 /* Use double quote and escape character:
